02f9d99ce82f1e1090ca7a808df2c3e0daa180d2
Artem MELNYK <artem.melnyk@celadodc-rswl.com>
2023-09-30 22:34:08 +0200

    initial commit

---
+++ app/src/main/java/fr/melnyk/makarbirthdayapplication/MainActivity.kt
@@ -0,0 +1,46 @@
+package fr.melnyk.makarbirthdayapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import fr.melnyk.makarbirthdayapplication.ui.theme.MakarBirthdayApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MakarBirthdayApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MakarBirthdayApplicationTheme {
+        Greeting("Android")
+    }
+}
